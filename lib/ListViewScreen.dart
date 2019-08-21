a2a572c1bd49c49976fd7e25d20cc751448c3dc1
xuejingfei <584093947@qq.com>
2019-08-21 09:22:37 +0800

    增加tabBar功能

--- lib/ListViewScreen.dart
+++ lib/ListViewScreen.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
 
 class ListViewScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("第二个页面")),
-        body: ListView.builder(
-            itemCount: 100,
-            itemExtent: 100,
-            itemBuilder: (BuildContext context, int index) => ListTile(
-                title: Text("title$index"), subtitle: Text("body $index"))));
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('listView 展示方式'),
+          bottom: TabBar(
+              labelColor: Colors.red,
+              unselectedLabelColor: Color(0xff666666),
+              labelStyle: TextStyle(fontSize: 16),
+              tabs: [
+                Tab(text: '列表1',),
+                Tab(text: '列表2',),
+                Tab(text: '列表3',)
+              ]
+          ),
+        ),
+        body: TabBarView(
+            children: <Widget>[
+              buildVerticalListView(),
+              buildHorizontalListView(),
+              buildListView(),
+        ]),
+      ),
+    );
   }
+
+  Widget buildVerticalListView() {
+    return ListView(
+        children: <Widget>[
+        ListTile(leading: Icon(Icons.mail),title: Text("mail")),
+        ListTile(leading: Icon(Icons.mail),title: Text("mail")),
+        ListTile(leading: Icon(Icons.mail),title: Text("mail")),
+    ]);
+  }
+
+  Widget buildHorizontalListView() {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      itemExtent: 140,
+      children: <Widget>[
+        Container(color: Colors.red),
+        Container(color: Colors.blue),
+        Container(color: Colors.black),
+        Container(color: Colors.green)
+      ],
+    );
+  }
+
+  Widget buildListView() {
+    return ListView.separated(
+        itemBuilder: (BuildContext context,int index) => ListTile(title: Text("titel:$index"),subtitle: Text("body:$index")),
+        separatorBuilder: (BuildContext context,int index) => Divider(color: Colors.red),
+        itemCount: 100,
+    );
+  }
+
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page1'),
     );
